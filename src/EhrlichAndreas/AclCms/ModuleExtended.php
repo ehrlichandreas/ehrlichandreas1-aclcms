c908263a98d900da5b94291554dd7f048ef8f441
Andreas Ehrlich <ehrlich.andreas@googlemail.com>
2015-01-06 10:26:45 +0100

    acl update

--- src/EhrlichAndreas/AclCms/ModuleExtended.php
+++ src/EhrlichAndreas/AclCms/ModuleExtended.php
@@ -398,6 +398,40 @@ class EhrlichAndreas_AclCms_ModuleExtended extends EhrlichAndreas_AclCms_Module
     }
     
     /**
+     * Returns true if and only if the Role has access to the Resource
+     *
+     * The $role and $resource parameters may be references to, or the string identifiers for,
+     * an existing Resource and Role combination.
+     *
+     * If either $role or $resource is null, then the query applies to all Roles or all Resources,
+     * respectively. Both may be null to query whether the ACL has a "blacklist" rule
+     * (allow everything to all). By default, Zend_Acl creates a "whitelist" rule (deny
+     * everything to all), and this method would return false unless this default has
+     * been overridden (i.e., by executing $acl->allow()).
+     *
+     * If a $privilege is not provided, then this method returns false if and only if the
+     * Role is denied access to at least one privilege upon the Resource. In other words, this
+     * method returns true if and only if the Role is allowed all privileges on the Resource.
+     *
+     * This method checks Role inheritance using a depth-first traversal of the Role registry.
+     * The highest priority parent (i.e., the parent most recently added) is checked first,
+     * and its respective parents are checked similarly before the lower-priority parents of
+     * the Role are checked.
+     *
+     * @param  Zend_Acl_Role_Interface|string     $role
+     * @param  Zend_Acl_Resource_Interface|string $resource
+     * @param  string                             $privilege
+     * @uses   Zend_Acl::isAllowed()
+     * @return boolean
+     */
+    public function isAllowed($role = null, $resource = null, $privilege = null, $rebuildAcl = false)
+    {
+        $acl = $this->getAclObject($rebuildAcl);
+        
+        return $acl->isAllowed($role, $resource, $privilege);
+    }
+    
+    /**
      * 
      * @param array $resource
      * @param array $resourceParent
